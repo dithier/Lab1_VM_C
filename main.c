18d0fdd0b4796825af9f9fd0d4fead775e7065e8
dithier <carter.ithier@gmail.com>
2025-08-06 20:32:00 -0400

    initial commit with readme and main file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  //  This includes the header file for
+                    //  the set of input  and output functions
+                    //  and variables in c
+
+// this is your main function where all your code runs
+int main() {
+    // your code here
+
+    // returning 0 means everything went ok
+    return 0;
+}
